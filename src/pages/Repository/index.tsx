7f9ec67046c192a0509f9fe035273cdacb895680
Larissa71 <larissakuffel@gmail.com>
2021-12-02 16:37:46 -0300

    Configuração de teste

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Card from '../../components/Card'
 
 const Repository: React.FC = () => {
     return(
-        <h3>Repository</h3>
+        <> 
+            <h3>Repository</h3>
+            <Link to={`/`}>Dashboard</Link>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </>
     )
 }
 
